e5545197ce25926624f51c88c1e4524fa2f3e43e
Sergey Ksenofontov <s.ksenofontov@sup.com>
2016-02-27 21:19:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// var ansi = require('ansi');
+var nodemon = require('nodemon');
+
+console.log('nodemon is: ', nodemon);
+
+// var cursor = ansi(process.stdout);
+
+// console.log('We are ready to beep!');
+
+// cursor.beep();
